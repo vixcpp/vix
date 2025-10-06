3d211b6e014ac2e097e38cf62d5b4c5ba1db3e95
softadastra <gaspardkirira9@gmail.com>
2025-10-06 10:51:27 +0300

    First test

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,18 +2,20 @@
 
 int main()
 {
-    Vix::App app;
+        Vix::App app;
 
-    app.get("/hello", [](auto &req, auto &res)
-            { res.json({{"message", "Hello, World!"}}); });
+        app.get("/hello", [](auto &req, auto &res)
+                { res.json({{"message", "Hello, World!"}}); });
 
-    app.get("/ping", [](auto &req, auto &res)
-            { res.text("pong"); });
+        app.get("/ping", [](auto &req, auto &res)
+                { res.text("pong"); });
 
-    app.get("/users/{id}", [](auto &req, auto &res, auto &params)
-            {
-    std::string id = params["id"]; 
-    res.json({{"user_id", id}}); });
+        app.get("/users/{id}", [](auto &req, auto &res, auto &params)
+                {
+        std::string id = params["id"];
+        res.json({{"user_id", id}}); });
 
-    app.run(8080);
+        app.run(8080);
+
+        return 0;
 }
